d210371aa20389e4a3d55045192e35670d91572c
Jacob Williams <jacobwilliams@JacobWilliamsMBP.local>
2016-09-20 09:41:46 -0400

    challenge day 2 week 3

---
+++ day2/selectorchallenge.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+        $('#buttonHideShow').click(function(){
+         $("#para1").toggle();
+  });
+})
+$(document).ready(function() {
+    $('h3').remove();
+});
+$(document).ready(function() {
+    $('#li1').remove();
+});
